dbebfe47b97597f283d2ec4cf556670eef89948a
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-12-14 18:51:33 +0100

    adding BuilderClient class with 3 instances to test the builder pattern

--- src/main/java/design/mechitoua/patterns/creational/builder/BuilderClient.java
+++ src/main/java/design/mechitoua/patterns/creational/builder/BuilderClient.java
@@ -8,5 +8,8 @@ public class BuilderClient {
         Laptop laptop2 = new Laptop.LaptopBuilder("16GB", "1TB", "i7").setGraphicsCardEnabled(true)
                 .setBluetoothEnabled(true).build();
         System.out.println("Laptop2 configuration: " + laptop2);
+
+        Laptop laptop3 = new Laptop.LaptopBuilder("8GB", "500GB", "i5").setBluetoothEnabled(true).build();
+        System.out.println("Laptop3 configuration: " + laptop3);
     }
 }

cb6bcffe9788296c0f5b0023812988e8250372fe
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-12-25 17:39:39 +0100

    Adding the Currency class to model a Real currency

---
+++ src/main/java/design/mechitoua/patterns/behavioural/chainofresponsability/atm/Currency.java
@@ -0,0 +1,14 @@
+package design.mechitoua.patterns.behavioural.chainofresponsability.atm;
+
+public class Currency {
+    private int amount;
+
+    public Currency(int amount) {
+        super();
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+}

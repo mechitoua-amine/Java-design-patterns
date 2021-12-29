887fefa2007a00e534168dd2daaf0925263870c7
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-12-29 15:46:50 +0100

    Added a Message class to model a simple message passed between the observer and the subject

---
+++ src/main/java/design/mechitoua/patterns/behavioural/observer/message/Message.java
@@ -0,0 +1,14 @@
+package design.mechitoua.patterns.behavioural.observer.message;
+
+public final class Message {
+    private String message;
+
+    public Message(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+}

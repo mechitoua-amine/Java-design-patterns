9052a03767c8fa507af8806012539011e2f44ccf
mehmet_emin <mechitoua_amine@protonmail.ch>
2021-12-30 16:28:47 +0100

    Added a Collection interface

---
+++ src/main/java/design/mechitoua/patterns/behavioural/iterator/collection/Collection.java
@@ -0,0 +1,8 @@
+package design.mechitoua.patterns.behavioural.iterator.collection;
+
+import design.mechitoua.patterns.behavioural.iterator.Iterator;
+
+public interface Collection {
+    public abstract Iterator getIterator();
+
+}
